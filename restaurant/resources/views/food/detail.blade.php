761cdacf772aa5dad27999d36a4bf7092d66a807
Debbie <410112018@gms.ndhu.edu.tw>
2020-07-02 22:47:48 +0800

    final

--- restaurant/resources/views/food/detail.blade.php
+++ restaurant/resources/views/food/detail.blade.php
@@ -3,17 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4"></div>
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">商品</div>
+                <img class="img-fluid" src="{{ asset('images') }}\{{ $food->image }}" alt="">
 
                 <div class="card-body">
 
-
-                    You are logged in!
                 </div>
             </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">介紹</div>
+                <div class="card-body">
+                    <p><h2>{{ $food->name }}</h2></p>
+                    <p class="lead">{{ $food->description }}</p>
+                    <p><h5>價錢:{{ $food->price }}</h5></p>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
